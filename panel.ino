092b9adb89ecb2fa83dd703bce8813d19be12f13
Alex_VV <62617575+AlexVV13@users.noreply.github.com>
2023-04-22 07:45:43 +0200

    Create panel.ino

---
+++ panel.ino
@@ -0,0 +1,264 @@
+#include <Keyboard.h>
+ 
+// Knopjes
+const int power = 13;
+const int dis1 = 2;
+const int dis2 = 3;
+const int gates = 4;
+const int restraints = 5;
+const int estop = 6;
+const int reset = 7;
+const int functie1 = 8;
+ 
+// Lampjes
+const int dis1l = 9;
+const int dis2l = 10;
+const int resetl = 11;
+const int functionl = 12;
+ 
+// Opties
+bool gatesOpen = true;
+bool resOpen = true;
+bool estopped = false;
+bool canDispatch = true;
+bool trainParked = true;
+bool lightTest = false;
+ 
+// Dispatch
+const long DispatchDelay = 5000;
+unsigned long DispatchStartTime = 0;
+ 
+// Ledjes
+unsigned long currentMillis;
+unsigned long previousBlinkMillis = 0;
+const long blinkInterval = 500;
+const long estopInterval = 250;
+ 
+int ledState = LOW;
+int fncState = LOW;
+int estopState = LOW;
+ 
+// Advance
+bool preLoad;
+
+// Keyboard
+bool keyboardState;
+ 
+void setup() {
+  // Pins knopjes
+  pinMode(power, INPUT_PULLUP);
+  pinMode(dis1, INPUT_PULLUP);
+  pinMode(dis2, INPUT_PULLUP);
+  pinMode(gates, INPUT_PULLUP);
+  pinMode(restraints, INPUT_PULLUP);
+  pinMode(estop, INPUT_PULLUP);
+  pinMode(reset, INPUT_PULLUP);
+  pinMode(functie1, INPUT_PULLUP);
+ 
+  // Pins lampjes
+  pinMode(dis1l, OUTPUT);
+  pinMode(dis2l, OUTPUT);
+  pinMode(resetl, OUTPUT);
+  pinMode(functionl, OUTPUT);
+}
+ 
+// E-stop reset
+void emergencyReset() {
+  estopped = false;
+  digitalWrite(resetl, LOW);
+  Serial.write("RESETTING...\n");
+  Keyboard.press(0x72);
+  delay(5000);
+  Serial.write("RESET!\n");
+  Keyboard.releaseAll();
+  delay(200);
+}
+ 
+// E-stop
+void emergency() {
+  Keyboard.press(0x73);
+  delay(200);
+  Keyboard.releaseAll();
+ 
+  estopped = true;
+  digitalWrite(resetl, HIGH);
+  digitalWrite(dis1l, LOW);
+  digitalWrite(dis2l, LOW);
+  Serial.write("E-STOPPED\n");
+ 
+  while (estopped == true) {
+    if (digitalRead(reset) == LOW && digitalRead(estop) == LOW) {
+      emergencyReset();
+    }
+    delay(300);
+  }
+}
+
+// Vrijgave
+void dispatch() {
+  Keyboard.press(' ');
+  Serial.write("DISPATCHED");
+  delay(5000);
+  Keyboard.releaseAll();
+  trainParked = false;
+  DispatchStartTime = currentMillis;
+  delay(10000);
+  trainParked = true;
+}
+
+// Functie/ advance
+void functie() {
+  Keyboard.press(KEY_RETURN);
+  Serial.write("ADVANCED");
+  Keyboard.releaseAll();
+  updateFunctie();
+}
+ \
+void updateFunctie() {
+  if (preLoad) {
+    preLoad = false;
+  } else {
+    preLoad = true;
+  }
+}
+ 
+// Poortjes
+void openGates() {
+  Serial.write("OPEN GATES\n");
+  Keyboard.press(KEY_RIGHT_ARROW);
+  gatesOpen = true;
+  canDispatch = false;
+  delay(2000);
+  Keyboard.releaseAll();
+}
+ 
+void closeGates() {
+  Serial.write("CLOSE GATES\n");
+  Keyboard.press(KEY_LEFT_ARROW);
+  gatesOpen = false;
+  canDispatch = true;
+  delay(2000);
+  Keyboard.releaseAll();
+}
+ 
+// Beugels
+void openRestraints() {
+  Serial.write("OPEN RESTRAINTS\n");
+  Keyboard.press(KEY_UP_ARROW);
+  resOpen = true;
+  canDispatch = false;
+  delay(1000);
+  Keyboard.releaseAll();
+}
+ 
+void closeRestraints() {
+  Serial.write("CLOSE RESTRAINTS\n");
+  Keyboard.press(KEY_DOWN_ARROW);
+  resOpen = false;
+  canDispatch = true;
+  delay(1000);
+  Keyboard.releaseAll();
+}
+
+// Update LED
+void updateLights() {
+  if (currentMillis - previousBlinkMillis >= blinkInterval) {
+    previousBlinkMillis = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+  }
+  if (preLoad && !estopped) {
+    digitalWrite(functionl, ledState);
+  } else {
+    digitalWrite(functionl, LOW);
+  }
+  if (canDispatch && !resOpen && !gatesOpen && !estopped && trainParked) {
+    digitalWrite(dis1l, ledState);
+    digitalWrite(dis2l, ledState);
+  } else {
+    digitalWrite(dis1l, LOW);
+    digitalWrite(dis2l, LOW);
+  }
+}
+ 
+void loop() {
+  currentMillis = millis();
+  // Power
+  if (digitalRead(power) == LOW) {
+    // Lamptest
+    if (!lightTest) {
+      digitalWrite(dis1l, HIGH);
+      digitalWrite(dis2l, HIGH);
+      digitalWrite(resetl, HIGH);
+      digitalWrite(functionl, HIGH);
+      delay(1000);
+      digitalWrite(dis1l, LOW);
+      digitalWrite(dis2l, LOW);
+      digitalWrite(resetl, LOW);
+      digitalWrite(functionl, LOW);
+      lightTest = true;
+    }
+    updateLights();
+    // Zet keyboard aan
+    if (!keyboardState) {
+      Serial.begin(9600);
+      Keyboard.begin();
+      keyboardState = true;
+    }
+    // Dispatch
+    if (digitalRead(dis1) == LOW && digitalRead(dis2) == LOW) {
+      if (!gatesOpen && !resOpen && !estopped) {
+        dispatch();
+        delay(1000);
+      } else {
+ 
+      }
+    }
+ 
+  // Functie
+  if (digitalRead(functie1) == LOW) {
+    if (!gatesOpen && !resOpen && !estopped) {
+      functie();
+    } else {
+ 
+    }
+  }
+
+  // Poortjes
+  if (digitalRead(gates) == HIGH && gatesOpen == false) {
+    openGates();
+  }
+ 
+  if (digitalRead(gates) == LOW && gatesOpen == true) {
+    closeGates();
+  }
+ 
+  // Beugels
+  if (digitalRead(restraints) == HIGH && resOpen == false) {
+    openRestraints();
+  }
+ 
+  if (digitalRead(restraints) == LOW && resOpen == true) {
+    closeRestraints();
+  }
+ 
+  // E-stop
+  if (digitalRead(estop) == HIGH && estopped == false) {
+    emergency();
+  }
+  } else {
+    // Zet alles uit
+    if (keyboardState) {
+      Keyboard.end();
+      keyboardState = false;
+    }
+    digitalWrite(dis1l, LOW);
+    digitalWrite(dis2l, LOW);
+    digitalWrite(resetl, LOW);
+    digitalWrite(functionl, LOW);
+    lightTest = false;
+  }
+}
